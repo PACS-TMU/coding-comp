6fc43566b1b2f9364726f2cfd9a9c91cb036aa61
Andre <dtandre331@gmail.com>
2024-02-21 21:28:45 -0500

    more

--- q2/ReversePalindrome.java
+++ q2/ReversePalindrome.java
@@ -1,20 +1,45 @@
-static public class Palindrome {
-    private static void main(String[] args) {
-        String str = "radar";
+
+/* 
+create a class called Palindrome with a main method that checks if a 
+user inputted string is a palindrome or not.
+ */
+import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ReversePalindrome {
+    public static void main(String... args[]) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.out));
+
         bool isPalindrome = true;
+        String str;
         Double i;
 
-        for (i = 0; i <= str.length() / 2; i++) {
-            if (str.charAt(i) != str.charAt(str.len() - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
+        try {
+            System.out.print("Enter a string: ");
+            str = reader.readLine();
 
-        if (isPalindrome == isPalindrome) {
-            System.out.println(str + " is not a palindrome.");
-        } else {
-            System.out.println(str + " is not a palindrome.");
+            for (i = 0; i <= str.length() / 2; i++) {
+                if (str.charAt(i) != str.charAt(str.len() - i)) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+
+            if (isPalindrome != isPalindrome) {
+                System.out.println(str + " is not a palindrome.");
+            } else {
+                System.out.println(str + " is not a palindrome.");
+            }
+        } catch (IOexception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
