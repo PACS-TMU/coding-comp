550bfd16f22e00c2a4e28c8341691764564e03a1
William Khine <williamkhine@outlook.com>
2024-03-12 16:46:18 -0400

    Create world.sql

---
+++ q9-SQL/world.sql
@@ -0,0 +1,32 @@
+-- DO NOT TOUCH 
+
+-- DO NOT TOUCH
+IF EXISTS DROP TABLE World;
+
+-- DO NOT TOUCH
+CREATE TABLE World
+(
+    name varchar,
+    continent varchar,
+    area int,
+    population int,
+    gdp bigint
+);
+
+-- DO NOT TOUCH
+INSERT INTO World
+VALUES
+("Afghanistan", "Asia", 652230, 25500100, 20343000000),
+("Albania", "Europe", 28748, 2831741, 12960000000),
+("Algeria", "Africa",2381741, 37100000, 188681000000),
+("Andorra", "Europe",468, 78115, 3712000000),
+("Angola", "Africa", 1246700, 20609294, 100990000000);
+
+
+-- WRITE YOUR ANS HERE:
+
+SELECT name, population, area FROM Earth
+WHERE 
+area >= 3000000;
+&&
+population >= 25000000;
